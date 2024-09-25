fa4f84ee52675aa693f5199913e33dc9cb1471c0
OGATA Hiromu <hiromukkun@gmail.com>
2024-09-25 14:30:01 +0900

    change:レイアウト変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <>
-      <h1 className="flex justify-center">ツクカツ</h1>
+    <div className="flex justify-center">
+      <h1>ツクカツ</h1>
       <h2>つくばのエンジニア志望学生のインターン探しをサポートするサイト</h2>
-    </>
+    </div>
   );
 }
