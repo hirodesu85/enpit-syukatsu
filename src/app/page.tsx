8fa6bf53a7cf7f60114fa77a606707111b77bd34
OGATA Hiromu <hiromukkun@gmail.com>
2024-09-25 13:55:38 +0900

    add:タイトル表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1 className="flex justify-center">Hello World!!</h1>
+    <>
+      <h1 className="flex justify-center">ツクカツ</h1>
+      <h2>つくばのエンジニア志望学生のインターン探しをサポートするサイト</h2>
+    </>  
   );
 }
