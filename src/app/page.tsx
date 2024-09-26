9d64ab3bc39f82f9eb906526ccb9b8f0f089b1fb
kenTATATA <ogaken.fantasy@gmail.com>
2024-09-26 17:00:01 +0900

    比較対象追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,6 +42,83 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <div className="border rounded-lg p-6 shadow-lg w-full max-w-md">
+        {/* 会社のロゴ */}
+        <div className="flex justify-center mb-4">
+        </div>
+        {/* 会社情報 */}
+        <div className="space-y-4">
+          <div>
+            <h3 className="font-semibold text-lg">[未経験でも大丈夫！] ホームページを作ってみよう！</h3>
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg">社名:</h3>
+            <p>株式会社Good</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg">職種:</h3>
+            <p>ホームページ制作</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg">時給:</h3>
+            <p>1800円 ~ 2500円</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg">勤務地:</h3>
+            <p>東京都港区</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg">勤務時間:</h3>
+            <p>フレックスタイム制(週合計12時間)</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg">求められること:</h3>
+            <ul className="list-disc pl-5">
+              <li>学習意欲があり成長したい人</li>
+              <li>使う言語はHTMLとCSSです</li>
+              <li>実務前にチュートリアルを用意してあるので、未経験でも安心して参加いただけます！</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="border rounded-lg p-6 shadow-lg w-full max-w-md">
+        {/* 会社のロゴ */}
+        <div className="flex justify-center mb-4">
+        </div>
+        {/* 会社情報 */}
+        <div className="space-y-4">
+          <div>
+            <h3 className="font-semibold text-lg">[やる気のある人募集！！]ホームページを作ってみよう！</h3>
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg">社名:</h3>
+            <p>enen株式会社</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg">職種:</h3>
+            <p>ホームページ制作</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg">時給:</h3>
+            <p>1200円 ~ 2000円</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg">勤務地:</h3>
+            <p>茨城県つくば市梅園5丁目34</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg">勤務時間:</h3>
+            <p>フレックスタイム制(週合計12時間)</p>
+          </div>
+          <div>
+            <h3 className="font-semibold text-lg">求められること:</h3>
+            <ul className="list-disc pl-5">
+              <li>学習意欲があり成長したい人</li>
+              <li>基本的なWebの知識がある人</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
